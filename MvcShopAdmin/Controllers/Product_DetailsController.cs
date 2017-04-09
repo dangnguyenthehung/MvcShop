7c228cbd27be85aa04d4e2882e3bfaec6834552e
Đặng Nguyễn Thế Hưng <dangnguyenthehung@gmail.com>
2017-04-09 19:31:42 +0700

    upload image to server

--- MvcShopAdmin/Controllers/Product_DetailsController.cs
+++ MvcShopAdmin/Controllers/Product_DetailsController.cs
@@ -7,6 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Model.Framework;
+using System.Collections;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Threading;
 
 namespace MvcShopAdmin.Controllers
 {
@@ -41,26 +46,94 @@ namespace MvcShopAdmin.Controllers
         {
             ViewBag.Brand_Id = new SelectList(db.Brands, "Id", "BrandName");
             ViewBag.ProductType_Id = new SelectList(db.ProductTypes, "Id", "TypeName");
+
+            var heatType = new List<SelectListItem>
+            {
+                new SelectListItem{ Text = "Lạnh", Value = "0"},
+                new SelectListItem{ Text = "Nóng lạnh", Value = "1"}
+            };
+            ViewBag.HeatType = new SelectList(heatType, "Value", "Text");
             return View();
         }
 
         // POST: Product_Details/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,ProductCode,ProductName,ProductDetail,OldPrice,NewPrice,ProductImages,HeatType,ProductWeight,ProductDimension,InsertDate,ProductOrder,ProductStatus,ProductType_Id,Brand_Id")] Product_Details product_Details)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Product_Details.Add(product_Details);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Create");
+        //    }
+
+        //    foreach (string file in Request.Files)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine(file);
+        //    }
+        //    ViewBag.Brand_Id = new SelectList(db.Brands, "Id", "BrandName", product_Details.Brand_Id);
+        //    ViewBag.ProductType_Id = new SelectList(db.ProductTypes, "Id", "TypeName", product_Details.ProductType_Id);
+        //    return View(product_Details);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,ProductCode,ProductName,ProductDetail,OldPrice,NewPrice,ProductImages,HeatType,ProductWeight,ProductDimension,InsertDate,ProductOrder,ProductStatus,ProductType_Id,Brand_Id")] Product_Details product_Details)
+        public ActionResult Create(Product_Details product_Details)
         {
+            foreach (string file in Request.Files)
+            {
+                var postedFile = Request.Files[file];
+
+                var res = UploadImgAsync(postedFile).ToString();
+                product_Details.ProductImages = res;
+
+
+                //System.Diagnostics.Debug.WriteLine(file);
+
+                //var fileName = postedFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+                //var filePath = HttpContext.Server.MapPath("~/Uploads/" + fileName);
+
+                //postedFile.SaveAs(filePath);
+            }
             if (ModelState.IsValid)
             {
                 db.Product_Details.Add(product_Details);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Create");
             }
-
+            
             ViewBag.Brand_Id = new SelectList(db.Brands, "Id", "BrandName", product_Details.Brand_Id);
             ViewBag.ProductType_Id = new SelectList(db.ProductTypes, "Id", "TypeName", product_Details.ProductType_Id);
             return View(product_Details);
+        }
+
+        private async Task<string> UploadImgAsync(HttpPostedFileBase file)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://mvcshopuploadimages.gear.host/api/Files/Upload");
+
+                //client.BaseAddress = new Uri("http://localhost:54777/api/Files/Upload");
+
+                using (var content = new MultipartFormDataContent())
+                {
+                    byte[] fileBytes = new byte[file.InputStream.Length + 1]; file.InputStream.Read(fileBytes, 0, fileBytes.Length);
+                    var fileContent = new ByteArrayContent(fileBytes);
+                    fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment") { FileName = file.FileName };
+                    content.Add(fileContent);
+                    var result = await client.PostAsync(client.BaseAddress, content);
+
+                    var url = result.Content.ReadAsStringAsync().Result;
+
+
+                    System.Diagnostics.Debug.WriteLine(url);
+
+                    return url;
+                }
+            }
         }
 
         // GET: Product_Details/Edit/5
@@ -131,6 +204,15 @@ namespace MvcShopAdmin.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private string formatUrl(string originalUrl)
+        {
+            int startIndex = originalUrl.IndexOf("www");
+            originalUrl = originalUrl.Substring(startIndex, originalUrl.Length - startIndex - 1).Replace("\\", "/").Replace("//", "/");
+            string newUrl = "http://" + originalUrl;
+
+            return newUrl;
         }
     }
 }
---
+++ ShopAPI/Controllers/UploadController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ShopAPI.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // GET: api/Upload
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //[Route("api/Files/Uploads")]
+        //public async Task<string> Post()
+        //{
+        //    try
+        //    {
+        //        var httpRequest = HttpContext.Current.Request;
+        //        if (httpRequest.Files.Count > 0)
+        //        {
+        //            foreach (string file in httpRequest.Files)
+        //            {
+        //                var postedFile = httpRequest.Files[file];
+
+        //                var fileName = postedFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+        //                var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + fileName);
+
+        //                postedFile.SaveAs(filePath);
+
+        //                //return "/Uploads/" + fileName;
+        //                return filePath;
+
+        //            }
+        //        }
+        //        else
+        //        {
+        //            System.Diagnostics.Debug.WriteLine("No file");
+        //        }
+
+        //    }
+        //    catch (Exception exeption)
+        //    {
+        //        return exeption.Message;
+        //    }
+        //    return "No files";
+        //}
+
+        [Route("api/Files/Upload")]
+        public async Task<string> PostFormData()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            if (System.Web.HttpContext.Current.Request.Files.Count > 0)
+            {
+                var file = System.Web.HttpContext.Current.Request.Files[0];
+
+                var fileName = file.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+                var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + fileName);
+
+                file.SaveAs(filePath);
+
+                System.Diagnostics.Debug.WriteLine(filePath);
+
+                return filePath;
+            }
+            else
+            {
+                return "No file";
+            }
+
+        }
+    }
+}

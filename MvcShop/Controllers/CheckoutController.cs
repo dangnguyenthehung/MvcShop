8c87987c46fd21708eb3d59c1881dd796969699c
Đặng Nguyễn Thế Hưng <dangnguyenthehung@gmail.com>
2017-04-17 01:19:01 +0700

    route to check out page
    - Create checkout info model

--- MvcShop/Controllers/CheckoutController.cs
+++ MvcShop/Controllers/CheckoutController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Object;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,8 @@ namespace MvcShop.Controllers
         // GET: Checkout
         public ActionResult Index()
         {
+            var cartList = (List<CartItem>)Session["CART_SESSION"];
+
             return View();
         }
     }
